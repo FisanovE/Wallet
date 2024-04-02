6fa2a98ef18a23cc2480cc8cb44a66a0aac3701b
FisanovE <fj1983@mail.ru>
2024-04-02 10:42:24 +0500

    Добавлена блокировка данных для предотвращения конфликтов параллелизма.

--- src/main/java/com/example/Wallet/service/impl/WalletServiceImpl.java
+++ src/main/java/com/example/Wallet/service/impl/WalletServiceImpl.java
@@ -9,8 +9,12 @@ import com.example.Wallet.model.Account;
 import com.example.Wallet.model.OperationType;
 import com.example.Wallet.repository.WalletRepository;
 import com.example.Wallet.service.WalletService;
+import jakarta.persistence.LockModeType;
 import lombok.AllArgsConstructor;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
@@ -27,6 +31,7 @@ public class WalletServiceImpl implements WalletService {
      * @throws @throws NotFoundException если категория не найдена
      * @throws UncorrectedParametersException если сумма средств на счету становится отрицательной после списания
      */
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Override
     public AccountDtoOut save(AccountDtoIn accountDtoIn) {
         Account account = walletRepository.findById(accountDtoIn.getWalletId()).orElseThrow(
@@ -49,6 +54,7 @@ public class WalletServiceImpl implements WalletService {
      * @return объект AccountDtoOut
      * @throws NotFoundException если категория не найдена
      */
+    @Lock(LockModeType.PESSIMISTIC_READ)
     @Override
     public AccountDtoOut getById(Long id) {
         Account account = walletRepository.findById(id).orElseThrow(
